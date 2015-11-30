860ae198bb5814eb1c2ee90669ff57b2304edce0
stranac <stranac@hotmail.com>
2015-11-30 20:13:40 +0100

    Added www.python-forum.org versions to the includes.

--- python-forum-code-higlight.user.js
+++ python-forum-code-higlight.user.js
@@ -9,6 +9,10 @@
 // @include        http://python-forum.org/posting.php*
 // @include        http://python-forum.org/mcp.php*
 // @include        http://python-forum.org/search.php*
+// @include        http://www.python-forum.org/viewtopic.php*
+// @include        http://www.python-forum.org/posting.php*
+// @include        http://www.python-forum.org/mcp.php*
+// @include        http://www.python-forum.org/search.php*
 // @run-at         document-end
 // @require        https://google-code-prettify.googlecode.com/svn/loader/prettify.js
 // @resource       PrettifyCSS https://raw.githubusercontent.com/stranac/misc/master/python-forum-code-dark.css
